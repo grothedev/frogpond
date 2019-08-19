6427dc2a559099eda3e0d5d3bfdb6ec1ea06f778
grothedev <grothedev@gmail.com>
2019-08-19 01:26:02 -0500

    added votes to apitest

--- resources/views/apitest.blade.php
+++ resources/views/apitest.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
 
+  <h3>File upload</h3>
   {!! Form::open(['url' => 'api/files', 'files' => true]) !!}
     {!! Form::file('f[]', ['multiple' => 'multiple']) !!}
     <button type = "submit">Submit</button>
@@ -10,6 +11,7 @@
 
   <br>
 
+  <h3>Croak post</h3>
   {!! Form::open(['url' => 'api/croaks', 'files' => true]) !!}
     content {!! Form::text('content') !!}
     type {!! Form::text('type') !!}
@@ -19,6 +21,15 @@
     tags {!! Form::text('tags') !!}
     <br>
     file/s {!! Form::file('f[]', ['multiple' => 'multiple']) !!}
+    <button type = "submit">Submit</button>
+  {!! Form::close() !!}
+
+  <br>
+
+  <h3>Vote cast</h3>
+  {!! Form::open(['url' => 'api/votes']) !!}
+    croak id {!! Form::text('croak id') !!}
+    up/down {!! Form::checkbox('checked=up; unchecked=down', 'v') !!}
     <button type = "submit">Submit</button>
   {!! Form::close() !!}
 
