bf9381c36acf112e828ece1e2779632f3927c880
grothedev <grothedev@gmail.com>
2019-08-19 12:29:28 -0500

    fix dupe check

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -28,7 +28,7 @@ class VoteController extends Controller
     {
         $ip = \Request::getClientIp(true);
         $croak = Croak::findOrFail($req->croak_id);
-        $dupe = Vote::where('ip', '=', $ip)->first();
+        $dupe = Vote::where('ip', '=', $ip)->where('croak_id', '=', $req_croak_id)->first();
         var_dump($dupe);
 
         if (is_null($croak)) return -1;
@@ -46,10 +46,8 @@ class VoteController extends Controller
             }
             $v->save();
             $croak->save();
-        } else {
-            return $croak['score'];
         }
-        
+        return $croak['score'];
     }
 
     /**
