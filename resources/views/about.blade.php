6407b07b651a19b5a203416f93ee29e0dff2becd
grothedev <grothedev@gmail.com>
2019-08-26 14:38:50 -0500

    updated about and motd

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class = "container">
     <h5>?</h5>
-    <p>FrogPond is an anonymous, tag-based, location-based, discussion board with threaded comments and the file sharing capabilities. It is a platform to enable people to talk about whatever they want, with people located in the same town or geographic region, with no restrictions and no concern for reputation.
+    <p>FrogPond is an anonymous, tag-based, location-based, discussion board with threaded comments and file sharing capabilities. It is a platform to enable people to talk about whatever they want, with people located in the same town or geographic region, with no restrictions and no concern for reputation.
         </p>
 
     <h5>How is it used?</h5>
@@ -13,6 +13,6 @@
     <p>I want this app to become the best that it can, and so I want to hear what people want it to be. I feel it has immense potential to become a platform for all sorts of things from simple discussion to tool libraries to public gardens and composting to live music gatherings to local government to pranks and games. So please tell me if you've found any bugs or have any suggestions for improvement or entirely new feature ideas. </p>
 
     <h5>What's with the frog theme?</h5>
-    <p>Some evening during the summer of 2018, I went for a walk at McFarland park and then sat by the pond. I noticed there was a frog sitting in the water close to the shore, then I imagined that all of the frogs in a pond spend their evenings "croakin' with the bros", with their heads poking out of the water. Then I thought that this app could be a sort of "pond" which would provide the human minds with a place to croak, or resonate, with each other at the end of the day, connected by their shared conceptual concerns as well as their geographic location. </p>
+    <p>Some evening during the summer of 2018, I went for a walk at a park and then sat by a pond. I noticed there was a frog sitting in the water close to the shore, then I imagined that all of the frogs in a pond spend their evenings "croakin' with the bros", with their heads poking out of the water. Then I thought that this app could be a sort of "pond" which would provide the human minds with a place to croak, or resonate, with each other at the end of the day, connected by their shared conceptual concerns as well as their geographic location. </p>
 </div>
 @endsection
