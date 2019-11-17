3bafcd318e5cd32444fce3d4a854a6700913aa00
grothedev <grothedev@gmail.com>
2019-11-17 01:01:56 -0600

    for some reason votes route was commented out

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::Resource('croaks', 'CroakController');
 Route::Resource('files', 'FileController');
 Route::Resource('tags', 'TagController');
-//Route::Resource('votes', 'VoteController');
+Route::Resource('votes', 'VoteController');
 Route::Resource('reports', 'ReportController');
 Route::get('motd', function(){
     return view('motd');
